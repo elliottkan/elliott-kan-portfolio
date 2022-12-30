406287c8f10b487190fb214156909114334c5237
Elliott Kan <elliottkan7@gmail.com>
2022-12-30 17:09:19 +1100

    Fixed Cursor Behavior not responding to A tags

--- src/components/cursor/cursor.tsx
+++ src/components/cursor/cursor.tsx
@@ -1,58 +1,48 @@
 import "./cursor.scss";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import useMousePosition from "../../hooks/useMousePosition";
 
-const Cursor: React.FC = () => {
-  // 1.
+const Cursor = () => {
+    // 1.
   const { x, y } = useMousePosition();
 
-  // 2. Use useRef to store a reference to the DOM elements
-  const ringRef = useRef<HTMLDivElement | null>(null);
-  const dotRef = useRef<HTMLDivElement | null>(null);
+  let ring = document.querySelector('.ring');
+  let dot = document.querySelector('.dot');
+  const a = document.querySelectorAll('a');
+  const burger = document.querySelectorAll('#burger');
 
-  // 3. Use useEffect to add and remove event listeners
-  useEffect(() => {
-    const a = document.querySelectorAll("a");
-    const burger = document.querySelectorAll("#burger");
-
-    function handleMouseOver() {
-      if (dotRef.current) dotRef.current.classList.add("hover");
-      if (ringRef.current) ringRef.current.classList.add("hidden");
-    }
-
-    function handleMouseLeave() {
-      if (dotRef.current) dotRef.current.classList.remove("hover");
-      if (ringRef.current) ringRef.current.classList.remove("hidden");
-    }
-
-    a.forEach(item => {
-      item.addEventListener("mouseover", handleMouseOver);
-      item.addEventListener("mouseleave", handleMouseLeave);
+  a.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      dot.classList.add('hover');
+      ring.classList.add('hidden');
     });
-
-    burger.forEach(item => {
-      item.addEventListener("mouseover", handleMouseOver);
-      item.addEventListener("mouseleave", handleMouseLeave);
+    item.addEventListener('mouseleave', () => {
+      dot.classList.remove('hover');
+      ring.classList.remove('hidden');
     });
+  })
 
-    // 4. Return a function that removes the event listeners when the component unmounts
-    return () => {
-      a.forEach(item => {
-        item.removeEventListener("mouseover", handleMouseOver);
-        item.removeEventListener("mouseleave", handleMouseLeave);
-      });
-
-      burger.forEach(item => {
-        item.removeEventListener("mouseover", handleMouseOver);
-        item.removeEventListener("mouseleave", handleMouseLeave);
-      });
-    };
-  }, []); // 5. Add an empty dependency array to only run this effect once when the component mounts
+  burger.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      dot.classList.add('hover');
+      ring.classList.add('hidden');
+    });
+    item.addEventListener('mouseleave', () => {
+      dot.classList.remove('hover');
+      ring.classList.remove('hidden');
+    });
+  })
 
   return (
     <>
-      <div ref={ringRef} style={{ left: `${x}px`, top: `${y}px` }} className="ring"></div>
-      <div ref={dotRef} className="dot" style={{ left: `${x}px`, top: `${y}px` }}></div>
+      <div
+        style={{ left: `${x}px`, top: `${y}px` }}
+        className="ring"
+      ></div>
+      <div
+        className="dot"
+        style={{ left: `${x}px`, top: `${y}px` }}
+      ></div>
     </>
   );
 };
