8f233a1ae2722ccaf13bc75635fed66a3f8d3bd0
Elliott Kan <elliottkan7@gmail.com>
2022-12-30 17:04:52 +1100

    NavBar Logic added  to Scroll Behavior

--- src/components/cursor/cursor.tsx
+++ src/components/cursor/cursor.tsx
@@ -1,48 +1,58 @@
 import "./cursor.scss";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import useMousePosition from "../../hooks/useMousePosition";
 
-const Cursor = () => {
-    // 1.
+const Cursor: React.FC = () => {
+  // 1.
   const { x, y } = useMousePosition();
 
-  let ring = document.querySelector('.ring');
-  let dot = document.querySelector('.dot');
-  const a = document.querySelectorAll('a');
-  const burger = document.querySelectorAll('#burger');
+  // 2. Use useRef to store a reference to the DOM elements
+  const ringRef = useRef<HTMLDivElement | null>(null);
+  const dotRef = useRef<HTMLDivElement | null>(null);
 
-  a.forEach(item => {
-    item.addEventListener('mouseover', () => {
-      dot.classList.add('hover');
-      ring.classList.add('hidden');
-    });
-    item.addEventListener('mouseleave', () => {
-      dot.classList.remove('hover');
-      ring.classList.remove('hidden');
-    });
-  })
+  // 3. Use useEffect to add and remove event listeners
+  useEffect(() => {
+    const a = document.querySelectorAll("a");
+    const burger = document.querySelectorAll("#burger");
 
-  burger.forEach(item => {
-    item.addEventListener('mouseover', () => {
-      dot.classList.add('hover');
-      ring.classList.add('hidden');
+    function handleMouseOver() {
+      if (dotRef.current) dotRef.current.classList.add("hover");
+      if (ringRef.current) ringRef.current.classList.add("hidden");
+    }
+
+    function handleMouseLeave() {
+      if (dotRef.current) dotRef.current.classList.remove("hover");
+      if (ringRef.current) ringRef.current.classList.remove("hidden");
+    }
+
+    a.forEach(item => {
+      item.addEventListener("mouseover", handleMouseOver);
+      item.addEventListener("mouseleave", handleMouseLeave);
     });
-    item.addEventListener('mouseleave', () => {
-      dot.classList.remove('hover');
-      ring.classList.remove('hidden');
+
+    burger.forEach(item => {
+      item.addEventListener("mouseover", handleMouseOver);
+      item.addEventListener("mouseleave", handleMouseLeave);
     });
-  })
+
+    // 4. Return a function that removes the event listeners when the component unmounts
+    return () => {
+      a.forEach(item => {
+        item.removeEventListener("mouseover", handleMouseOver);
+        item.removeEventListener("mouseleave", handleMouseLeave);
+      });
+
+      burger.forEach(item => {
+        item.removeEventListener("mouseover", handleMouseOver);
+        item.removeEventListener("mouseleave", handleMouseLeave);
+      });
+    };
+  }, []); // 5. Add an empty dependency array to only run this effect once when the component mounts
 
   return (
     <>
-      <div
-        style={{ left: `${x}px`, top: `${y}px` }}
-        className="ring"
-      ></div>
-      <div
-        className="dot"
-        style={{ left: `${x}px`, top: `${y}px` }}
-      ></div>
+      <div ref={ringRef} style={{ left: `${x}px`, top: `${y}px` }} className="ring"></div>
+      <div ref={dotRef} className="dot" style={{ left: `${x}px`, top: `${y}px` }}></div>
     </>
   );
 };
--- src/components/navbar/NavLinks.tsx
+++ src/components/navbar/NavLinks.tsx
@@ -1,35 +1,47 @@
-import React from 'react'
+import React from "react";
 
-const NavLinks = (props) => {
+const NavLinks: React.FC = () => {
 
-  const SectionLinks = () => {
+  const SectionLinks: React.FC = () => {
     return (
       <ul>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#work">Work</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#work">Work</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
-    )
-  }
+    );
+  };
 
-  const ResumeLink = () => {
+  const ResumeLink: React.FC = () => {
     return (
       <div>
-        <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+        <a
+          className="resume-button"
+          href="/resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Resume
         </a>
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <>
-      <SectionLinks/>
+      <SectionLinks />
       <ResumeLink />
     </>
-  )
+  );
+};
 
-}
-
-export default NavLinks
+export default NavLinks;
