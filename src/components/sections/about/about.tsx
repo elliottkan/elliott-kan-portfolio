07c331d9b9afd7d60f75e46882c0424075274b5e
Elliott Kan <elliottkan7@gmail.com>
2023-01-03 21:10:27 +1100

    Styling Updates for About

--- src/components/sections/about/about.tsx
+++ src/components/sections/about/about.tsx
@@ -8,9 +8,10 @@ const About = () => {
     <div className="about-container">
       <div className="text-container">
         <h2>Who in the world is Elliott?</h2>
+        <p></p>
       </div>
       <div className="photo-container">
-        
+        <img src="" alt="elliott-headshot" />
       </div>
     </div>
   )
