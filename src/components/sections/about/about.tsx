84d70b8f876a942c39c77b233ba339d8b5391b50
Elliott Kan <elliottkan7@gmail.com>
2023-01-06 21:53:27 +1100

    hero 70vh ILO 90vh

--- src/components/sections/about/about.tsx
+++ src/components/sections/about/about.tsx
@@ -9,7 +9,10 @@ const About = () => {
     <div className="about-container">
       <div className="text-container">
         <h2>Who in the world is Elliott?</h2>
-        <p>I love coding man</p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
       </div>
       <div className="photo-container">
         <img src={Headshot} alt="handsome man headshot" />
