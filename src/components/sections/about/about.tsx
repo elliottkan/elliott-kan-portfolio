59880ef1b7df35d969580db1c0c33c0c8bf49944
Elliott Kan <elliottkan7@gmail.com>
2023-01-06 22:05:26 +1100

    Information added

--- src/components/sections/about/about.tsx
+++ src/components/sections/about/about.tsx
@@ -9,10 +9,10 @@ const About = () => {
     <div className="about-container">
       <div className="text-container">
         <h2>Who in the world is Elliott?</h2>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
+        <p>Hey there!</p>
+        <p>I'm a software developer with a love for coding and problem-solving. When I'm not working on projects, you can find me indulging my inner outdoor enthusiast through rock climbing and exploring the great outdoors. I'm an avid rock climber and love nothing more than getting out into nature and pushing myself to new heights (both literally and figuratively).</p>
+        <p>In my free time, I also enjoy reading self-help books and constantly seeking out new knowledge and skills to learn. I bring a laid back and casual vibe to everything I do and always try to find the humor in life's journey.</p>
+        <p>Overall, I'm a driven and dedicated individual who is always looking for ways to improve myself and those around me. I have a passion for creating solutions that not only contribute to a more positive and sustainable future, but also bring joy and functionality to the users. Whether I'm tackling a difficult coding challenge or scaling a rock face, I approach everything with determination and a positive attitude.</p>
       </div>
       <div className="photo-container">
         <img src={Headshot} alt="handsome man headshot" />
