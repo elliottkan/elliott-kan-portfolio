0bfba8086599669e27d7cc017c4f52cfce70d1d3
Elliott Kan <elliottkan7@gmail.com>
2023-01-24 16:20:57 +1100

    NavBar has now been refactored

--- src/components/cursor/cursor.tsx
+++ src/components/cursor/cursor.tsx
@@ -1,54 +1,3 @@
-// import "./cursor.scss";
-// import React from "react";
-// import useMousePosition from "../../hooks/useMousePosition";
-
-// const Cursor = () => {
-//     // 1.
-//   const { x, y } = useMousePosition();
-
-//   let ring = document.querySelector('.ring');
-//   let dot = document.querySelector('.dot');
-//   const a = document.querySelectorAll('a');
-//   const burger = document.querySelectorAll('#burger');
-
-//   a.forEach(item => {
-//     item.addEventListener('mouseover', () => {
-//       dot.classList.add('hover');
-//       ring.classList.add('hidden');
-//     });
-//     item.addEventListener('mouseleave', () => {
-//       dot.classList.remove('hover');
-//       ring.classList.remove('hidden');
-//     });
-//   })
-
-//   burger.forEach(item => {
-//     item.addEventListener('mouseover', () => {
-//       dot.classList.add('hover');
-//       ring.classList.add('hidden');
-//     });
-//     item.addEventListener('mouseleave', () => {
-//       dot.classList.remove('hover');
-//       ring.classList.remove('hidden');
-//     });
-//   })
-
-//   return (
-//     <>
-//       <div
-//         style={{ left: `${x}px`, top: `${y}px` }}
-//         className="ring"
-//       ></div>
-//       <div
-//         className="dot"
-//         style={{ left: `${x}px`, top: `${y}px` }}
-//       ></div>
-//     </>
-//   );
-// };
-
-// export default Cursor;
-
 import "./cursor.scss";
 import React, { useEffect, useRef } from "react";
 import useMousePosition from "../../hooks/useMousePosition";
@@ -91,7 +40,7 @@ const Cursor = () => {
         item.removeEventListener("mouseleave", handleMouseLeave);
       });
     };
-  }, []);
+  }, [location.pathname]);
 
   return (
     <>
